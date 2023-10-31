f00267ddd70259dc689be1995b1adb1ddec3da72
Jericho Hasselbush <jericho@sal-et-lucem.com>
2023-10-31 08:05:24 -0400

    Able to open file with multiples of same type of component.

--- Resistor/Persistence/CircuitComponent+Codable.swift
+++ Resistor/Persistence/CircuitComponent+Codable.swift
@@ -17,47 +17,38 @@ extension Circuit: Codable {
 
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-//        var componentContainer = container.nestedContainer(keyedBy: ComponentKeys.self, forKey: .components)
-        var componentContainer = container.nestedUnkeyedContainer(forKey: .components)
+        var componentContainer = container.nestedContainer(keyedBy: ComponentKeys.self, forKey: .components)
+//        var componentContainer = container.nestedUnkeyedContainer(forKey: .components)
         for key in ComponentKeys.allCases {
             let sameComps = components.compactMap({ component in
                 component.key == key ? key.magic(component) : nil
             })
-//            try componentContainer.encode(sameComps, forKey: key)
-            try componentContainer.encode(sameComps)
+            try componentContainer.encode(sameComps, forKey: key)
+//            try componentContainer.encode(sameComps)
         }
-
-
-
-//        var compContiner = container.nestedContainer(keyedBy: ComponentKeys.self, forKey: .components)
-//        for component in self.components {
-//            print("Component: \(component)")
-//            try compContiner.encode(component, forKey: component.key)
-//        }
-//        print("Container: \(compContiner)")
     }
 
     init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        var compContainer = try container.nestedUnkeyedContainer(forKey: .components)
-
-        let comps = [Component]()
-        while !compContainer.isAtEnd {
-            let comp = try compContainer.decodeIfPresent(Node.self)
-        }
-
-        let componentContainer = try container.nestedContainer(keyedBy: ComponentKeys.self, forKey: .components)
         var components = [Component]()
-        for key in componentContainer.allKeys {
-            switch key {
-            case .node:
-                components.append(try componentContainer.decode(Node.self, forKey: .node))
-            case .wire:
-                components.append(try componentContainer.decode(Wire.self, forKey: .wire))
-            case .resistor:
-                components.append(try componentContainer.decode(Resistor.self, forKey: .resistor))
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let componentContainer = try container.nestedContainer(keyedBy: ComponentKeys.self, forKey: .components)
+        for key in ComponentKeys.allCases {
+            let things = try componentContainer.decode([ComponentEnum].self, forKey: key)
+            print(things)
+            if !things.isEmpty {
+                for thing in things {
+                    switch thing {
+                    case .node(let node):
+                        components.append(node)
+                    case .wire(let wire):
+                        components.append(wire)
+                    case .resistor(let resistor):
+                        components.append(resistor)
+                    }
+                }
             }
         }
+
 
         self = Circuit(components: components)
     }
