4aa43440099c9c40ab32e3afc80c297e5ec0a292
Jericho Hasselbush <jericho@sal-et-lucem.com>
2023-11-04 21:49:21 -0400

    Moved model tests to own file.

--- ResTexTests/CircuitTests.swift
+++ ResTexTests/CircuitTests.swift
@@ -24,30 +24,4 @@ final class CircuitTests: XCTestCase {
         XCTAssertEqual(sut.components.count, 2)
     }
 
-    func test_shouldSelectComponent() throws {
-        let circuit = Circuit(components: [node, wire, resistor])
-        let sut = ResTexModel(circuit: circuit, callback: { _ in })
-        sut.selectComponent(node)
-        XCTAssertEqual(sut.selectedComponent?.id, node.id)
-    }
-
-    func test_shouldAddComponent() throws {
-        let res2 = Resistor(start: .init(x: 10, y: 10))
-        let sut = ResTexModel(circuit: .init(), callback: { _ in })
-        sut.addComponent(res2)
-        XCTAssertEqual(sut.circuit.components.count, 1)
-    }
-
-    func test_shouldSelectResistor() throws {
-        let sut = ResTexModel(circuit: .init(components: [wire, resistor]), callback: { _ in })
-        let comp = sut.handleTapAtPoint(point: resistor.start)
-        XCTAssertEqual(comp?.id, resistor.id)
-    }
-
-    func test_shouldDeleteComponent() throws {
-        let sut = ResTexModel(circuit: .init(components: [node, wire, resistor]), callback: { _ in })
-        let component = sut.handleTapAtPoint(point: resistor.start)
-        sut.deleteComponent(component!)
-        XCTAssertEqual(sut.circuit.components.count, 2)
-    }
 }
